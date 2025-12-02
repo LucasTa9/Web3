33e091d1e1fafc628416f872593a91e900bc5565
LucasTa9 <lucastadeu3369@gmail.com>
2025-12-02 00:51:32 -0300

    Delete petshop directory

--- petshop/src/main/java/br/edu/ifspcjo/ads/web3/petshop/repository/PetRepository.java
+++
@@ -1,9 +0,0 @@
-package br.edu.ifspcjo.ads.web3.petshop.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.edu.ifspcjo.ads.web3.petshop.domain.model.Pet;
-
-public interface PetRepository extends JpaRepository<Pet, Long>{
-
-}
-
